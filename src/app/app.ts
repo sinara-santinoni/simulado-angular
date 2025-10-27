fb54efdbd9dfbd9a54f7ce56a745f5c6ba00c76b
sinara_santinoni <sinara_santinoni@hotmail.com>
2025-10-27 16:20:17 -0300

    inicio do card dos jogadores

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './componentes/navbar/navbar';
 import { BarraNavegacao } from './componentes/barra-navegacao/barra-navegacao';
+import { Cards } from "./componentes/cards/cards";
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,BarraNavegacao],
+  imports: [RouterOutlet, Navbar, BarraNavegacao, Cards],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
